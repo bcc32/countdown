3369e1fac02898f98ac57706845cd8398629f4ac
Aaron L. Zeng <a2z@mit.edu>
2017-08-02 11:56:59 +0200

    Add some doc comments to Expr

--- lib/expr.mli
+++ lib/expr.mli
@@ -1,10 +1,20 @@
 open Core
 
+(** The type of Countdown mathematical expressions.
+
+    Allowed operations are [+], [-], [*], and [/], and only non-negative
+    integers are allowed as intermediate results. *)
 type t [@@deriving sexp_of]
 
+(** [literal n] returns an expression which is just the integer literal [n]. *)
 val literal : int -> t
-val value   : t   -> int
 
+val value : t -> int
+
+(** [all_combinations a b] returns a list of allowed operations on [a] and [b],
+    excluding any duplications of the same resulting value. *)
 val all_combinations : t -> t -> t list
 
+(** Expressions compare by value, so two expressions whose results are the same
+    but whose structures are different compare equal. *)
 include Comparable.S with type t := t
