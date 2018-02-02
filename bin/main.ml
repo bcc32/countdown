fa8504a3813fa909e5bcf6af8af63fc62bb63436
Aaron L. Zeng <me@bcc32.com>
2018-02-02 06:07:48 -0500

    Style

--- bin/main.ml
+++ bin/main.ml
@@ -4,8 +4,7 @@ open Countdown
 let command =
   Command.group ~summary:"Countdown solver"
     [ "numbers", Numbers.command
-    ; "words", Words.command
-    ]
+    ; "words",   Words.command ]
 ;;
 
 let () = Command.run command
